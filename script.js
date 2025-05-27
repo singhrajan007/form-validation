0f3afa08e3da7e50abcf19bc723d3f850d142380
Rajan Singh <76697406+singhrajan007@users.noreply.github.com>
2025-05-27 08:40:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
             // Validate Password
             const password = /^(?=.*[A-Za-z])(?=.*[-!@._*#%])(?=.*\d)[A-Za-z\d-!@._*#%]{8,16}$/;
             if (!password.test($("#password").val())) {
-                error += "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.\n";
+                error += "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one special character, and one number.\n";
                 isValid = false;
             }
                 
